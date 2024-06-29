60a60d5bf3ab28716de150de68bc80be00f2e652
Juan Diego Teheran Olmos <jdteheran@uninorte.edu.co>
2024-06-29 08:30:14 -0500

    actualizar

--- app.js
+++ app.js
@@ -1,65 +1,20 @@
 import express from 'express'
-import mongoose from 'mongoose'
-import { Schema, model } from 'mongoose'
+import connection from './database/db.js'
+import router from './routes/main.route.js'
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
 
-const userdb = process.env.USERDB
-const passdb = process.env.PASSDB
-const hostdb = process.env.HOSTDB
-const namedb = process.env.NAMEDB
+await connection()
 
-const url = `mongodb+srv://${userdb}:${passdb}@${hostdb}/${namedb}`
+app.use('/api', router)
 
-try {
-  await mongoose.connect(url)
-  console.log('Database connected');
-} catch (error) {
-  console.log('Error Database', error);
-}
-
-const user_schema = Schema({
-  name: {
-    type: String,
-    require: true
-  },
-  nickname: {
-    type: String,
-    require: true
-  },
-  cel:{
-    type: String,
-    require: true
-  },
-  password:{
-    type: String,
-    require: true
-  },
-  email:{
-    type: String
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+app.get('*', (req, res) => {
+  res.status(404).json({
+    msg: 'not found'
+  })
 })
-
-const user = model('user', user_schema)
-
-const first_user = new user({
-  name: 'Jhon',
-  nickname: 'jjatt',
-  cel:'3214567898',
-  password:'123'
-})
-
-try {
-  await first_user.save()
-} catch (error) {
-  console.log(error);
-}
 
 app.listen(port, () => console.log(`Server on port: ${port}`))
